d93eb48351bc055eb8cbc39e407d3c5dbed360de
Drew Colthorp <colthorp@atomicobject.com>
2017-04-21 12:38:20 -0400

    Add a delay

--- modules/client/sagas/index.test.ts
+++ modules/client/sagas/index.test.ts
@@ -1,11 +1,22 @@
 import {foo, callAFunctionSaga} from './index'
-import {call} from 'redux-saga/effects';
+import {call, put} from 'redux-saga/effects';
+import {delay} from 'redux-saga';
 
 describe("a saga", () => {
   it('adds 1 + 4 to equal 3 in TScript', () => {
     let generator = callAFunctionSaga();
+    let nextEffect;
 
-    expect(generator.next().value).toEqual(call(foo, 3))
+    nextEffect = generator.next();
+
+    expect(nextEffect.value).toEqual(call(foo, 3))
+    nextEffect = generator.next("asdf")
+
+    expect(nextEffect.value).toEqual(call(delay, 100));
+    nextEffect = generator.next()
+
+    expect(nextEffect.value).toEqual(put({type: 'gotResult', value: 'asdf'}));
+
     expect(generator.next().done).toBeTruthy();
   });
 })
--- modules/client/sagas/index.ts
+++ modules/client/sagas/index.ts
@@ -1,5 +1,6 @@
 import {SagaIterator} from 'redux-saga';
-import {call} from 'redux-saga/effects';
+import {call, put} from 'redux-saga/effects';
+import {delay} from 'redux-saga'
 
 export function foo(x:number) : string {
   return null as any;
@@ -7,6 +8,8 @@ export function foo(x:number) : string {
 
 export function* callAFunctionSaga() : SagaIterator {
   const x: string = yield call(foo, 3);
+  yield call(delay, 100);
+  yield put({type: 'gotResult', value: x});
 }
 
 export function* rootSaga() : SagaIterator {
