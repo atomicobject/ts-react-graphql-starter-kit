a1c0d0bbf07edaeda49b961d7913ea9bfe4605ca
Drew Colthorp <colthorp@atomicobject.com>
2017-04-21 15:45:25 -0400

    Get somer reducers and state in place

--- modules/client/index.tsx
+++ modules/client/index.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Route,Link} from 'react-router-dom'
+import {ConnectedRouter} from 'react-router-redux';
+import { Route,Link} from 'react-router-dom'
 
 import {RedBox} from './components/red-box';
+import {History} from 'history';
 
 function Home() {
   return <div>
@@ -23,11 +25,11 @@ class App extends React.Component<{},{}> {
   }
 }
 
-export default function Root() {
-  return <Router>
+export default function Root(props: {history: History}) {
+  return <ConnectedRouter history={props.history}>
     <App>
       <Route exact path="/" component={Home} />
       <Route path="/foo" component={Foo} />
     </App>
-  </Router>;
+  </ConnectedRouter>;
 }
