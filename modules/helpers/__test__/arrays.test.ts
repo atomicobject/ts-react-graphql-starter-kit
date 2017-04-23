525d0be93cd2f5df6870289f271c3e878fae5758
Drew Colthorp <colthorp@atomicobject.com>
2017-04-23 15:29:20 -0400

    Arrays.shift, doc comments, jsverify for property testing

--- modules/helpers/__test__/arrays.test.ts
+++ modules/helpers/__test__/arrays.test.ts
@@ -1,4 +1,7 @@
 import {Arrays} from '../arrays';
+import * as jsv from 'jsverify';
+import {isEqual} from 'lodash';
+
 describe('Arrays', () => {
   describe('index', () => {
     it('is a prism for the specified index', () => {
@@ -21,7 +24,7 @@ describe('Arrays', () => {
   });
 
   describe('.splice', () => {
-    it('works like Array#splice but copies', () => {
+    it('works on some examples', () => {
       const splice = Arrays.splice;
 
       const splice1 = [1,2,4]
@@ -39,7 +42,20 @@ describe('Arrays', () => {
       //more examples
       expect(splice([1, 2, 3], 1, 1, 4, 5)).toEqual([1,4,5,3]);
       expect(splice([1, 2, 3], 1, 2)).toEqual([1]);
-    })
+    });
+
+    it('really is like Array#splice', () => {
+      var prop = jsv.forall('array nat', 'nat', 'nat', 'array nat',
+        (a: number[], i: number, del: number, add: number[]) => {
+          const nativeResult = a.slice();
+          nativeResult.splice(i, del, ...add);
+
+          const ourResult = Arrays.splice(a, i, del, ...add);
+          return isEqual(nativeResult, ourResult);
+        });
+
+        jsv.check(prop, {tests: 100})
+    });
   });
 
   describe('.push', () => {
@@ -73,6 +89,18 @@ describe('Arrays', () => {
       expect(unshift(a,1)).toEqual([1, 0]);
       expect(unshift(a,2)).toEqual([2, 0]);
       expect(a).toEqual([0]);
+    })
+  });
+
+  describe('.shift', () => {
+    it('works like Array#shift but copies', () => {
+      const shift = Arrays.shift;
+
+      const a : number[] = [0, 1];
+      expect(shift(a)).toEqual([1]);
+      expect(a).toEqual([0, 1]);
+
+      expect(shift([])).toEqual([])
     })
   });
 })
