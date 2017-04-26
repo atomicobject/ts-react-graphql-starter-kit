db268d21aaae1963d0f7ba23418cdec67551f69e
Rachael Miller <rachael.miller@atomicobject.com>
2017-04-26 09:24:46 -0400

    Restart the game with new numbers on completion

--- modules/client/sagas/index.ts
+++ modules/client/sagas/index.ts
@@ -1,12 +1,14 @@
-import {SagaIterator} from 'redux-saga';
-import {call, put, takeLatest, take, fork, select} from 'redux-saga/effects';
-import {delay} from 'redux-saga'
-import {ActionTypes,
-        GuessSubmittedAction, 
-        answerChanged,
-        goodGuessOccurred, 
-        badGuessOccurred, 
-        gameWon} from '../actions'
+import { SagaIterator } from 'redux-saga';
+import { call, put, takeLatest, take, fork, select } from 'redux-saga/effects';
+import { delay } from 'redux-saga'
+import {
+  ActionTypes,
+  GuessSubmittedAction,
+  answerChanged,
+  goodGuessOccurred,
+  badGuessOccurred,
+  gameWon
+} from '../actions'
 import { State } from '../state'
 
 import { graphqlClient } from '../graphql-client';
@@ -14,55 +16,53 @@ import gql from 'graphql-tag';
 
 import { Answer } from '../../graphql/types';
 
-export async function foo(x:number) : Promise<string> {
+export async function foo(x: number): Promise<string> {
   console.log("called me")
   return "Howdy!";
 }
 
-export function* callAFunctionSaga() : SagaIterator {
+export function* callAFunctionSaga(): SagaIterator {
   const x: string = yield call(foo, 3);
   yield call(delay, 100);
-  yield put({type: 'gotResult', value: x});
+  yield put({ type: 'gotResult', value: x });
 }
 
 export async function fetchAnswers(): Promise<Answer> {
   const result = await graphqlClient.query<{ answer: Answer }>({
     query: gql`{ answer }`,
+    fetchPolicy: "network-only"
   });
 
   return result.data.answer;
 }
 
-export function* gameSaga() : SagaIterator {
-  let guessedRight = false;  
+export function* gameSaga(): SagaIterator {
+  while (true) {
+    let guessedRight = false;
+    const newAnswer = yield call(fetchAnswers)
+    yield put(answerChanged(newAnswer))
 
-  const newAnswer = yield call(fetchAnswers)
-  yield put(answerChanged(newAnswer))
-  console.log(newAnswer);
+    while (!guessedRight) {
+      let currentGuess = 0;
 
-  while(!guessedRight) {
-    let currentGuess = 0; 
-
-    const rightAnswer: Answer = yield select(State.answerSequence);
-    for(; currentGuess < rightAnswer.length; currentGuess++) {
-      const guess: GuessSubmittedAction = yield take(ActionTypes.GUESS_SUBMITTED);
-      if (guess.value === rightAnswer[currentGuess]) {
-        yield put(goodGuessOccurred(guess.value));
-      } else {
-        yield put(badGuessOccurred(guess.value));
-        break;
-      } 
-    }
-    if (currentGuess === rightAnswer.length) {
-      yield put(gameWon(rightAnswer));
-      guessedRight = true;
-      // yield put(beginGame())
-      // CALLOUT: Should we loop back to 39 here OR
-      // end and restart the saga?
+      const rightAnswer: Answer = yield select(State.answerSequence);
+      for (; currentGuess < rightAnswer.length; currentGuess++) {
+        const guess: GuessSubmittedAction = yield take(ActionTypes.GUESS_SUBMITTED);
+        if (guess.value === rightAnswer[currentGuess]) {
+          yield put(goodGuessOccurred(guess.value));
+        } else {
+          yield put(badGuessOccurred(guess.value));
+          break;
+        }
+      }
+      if (currentGuess === rightAnswer.length) {
+        yield put(gameWon(rightAnswer));
+        guessedRight = true;
+      }
     }
   }
 }
 
-export function* rootSaga() : SagaIterator {
+export function* rootSaga(): SagaIterator {
   yield fork(gameSaga);
 }
