b9893069c76bbbdf7b9362cc22d6c3de55363a42
Rachael Miller <rachael.miller@atomicobject.com>
2017-06-07 11:01:43 -0400

    Applied base styles to styleguide

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,5 +1,9 @@
+const path = require('path');
 module.exports = {
   components: "modules/client/components/**/*.tsx",
   webpackConfig: require('./webpack/client.config.js'),
   propsParser: require('react-docgen-typescript').parse,
+  require: [
+    './modules/client/styles/main.scss',
+  ]
 };
