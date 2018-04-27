ea1076bebba7bc172119f7f134937515ee7beea8
Drew Colthorp <colthorp@atomicobject.com>
2018-04-27 11:31:03 -0400

    fix jest default

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
+  forceExit: true,
   transform: {
     "\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/ts-jest/preprocessor.js",
     "\\.(gql|graphql)$": "jest-transform-graphql"
   },
   transformIgnorePatterns: ["node_modules\\/(?!(lodash-es|react-apollo)\\/)"],
-  testRegex: ".*\\.(test|spec)\\.(ts|tsx)$",
+  testRegex: "modules/.*\\.(test|spec)\\.(ts|tsx)$",
   moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json"],
   moduleNameMapper: {
     "\\.(css|less|scss)$": "identity-obj-proxy",
